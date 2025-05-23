f95eee6fb18d9942d5c822a7f86a212a82fa534c
karishma0815 <u23538199@tuks.co.za>
2025-05-23 19:59:35 +0200

    for logging out

---
+++ logout.php
@@ -0,0 +1,22 @@
+<?php
+session_start();
+$_SESSION = [];
+
+if (ini_get("session.use_cookies")) {
+    $params = session_get_cookie_params();
+    setcookie(session_name(), '', time() - 42000,
+        $params["path"], $params["domain"],
+        $params["secure"], $params["httponly"]
+    );
+}
+
+session_destroy();
+echo '
+<script>
+    localStorage.removeItem("apiKey");
+    document.cookie = "apiKey=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;";
+    window.location.href = "index.php";
+</script>
+';
+exit();
+?>
